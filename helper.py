308f4604498aa9fba3f8b1fe33e94e1a90440bcf
tommytunes <thomasstaples99@gmail.com>
2024-09-25 12:11:35 -0400

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,62 @@
+special_coins_pos = [(1, 1), (14, 1), (1, 13), (14, 13)]
+center_pos = [(12, 7), (11, 7), (13, 7), (14, 7)]
+
+def create_board():
+
+    # TODO Create a board with the following structure
+    # 1 -> Wall
+    # 0 -> Path
+    
+    maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Top boundary
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], # Path
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1], # Internal walls
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1], # Paths
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1], # Complex paths
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1], # Open path area
+    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1], # Narrow passage
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1], # Large open path
+    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], # Solid wall section
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], # Path
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1], # Complex wall structure
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1], # More paths
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1], # Internal walls
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], # Open path
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Extra boundary row
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Extra boundary row
+    ]
+
+    return maze
+
+def create_coins(board):
+    coins = []
+    
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[j][i] == 0:
+                coins.append((i,j))
+                
+    
+    # TODO: Ajouter la position de toutes les cases '0' à la variable coins. Pour ajouter un élément, vous pouvez utiliser l'expression suivante :
+    # coins.append((x, y))
+    # en remplacant x et y par la position. Notez que le premier coin est à la position (1, 1)
+    for positions_corners in special_coins_pos:
+        if positions_corners in special_coins_pos:
+            coins.remove(positions_corners)
+    # TODO: Retirer les coins de chaque "coin" du carré. Vous devez utiliser la variable 'special_coins_pos' et la fonction 'remove'.
+
+    # TODO: Retirer les coins aux positions centrales, en utilisant la variable 'center_pos'.
+    for positions_center in center_pos:
+        if positions_corners in center_pos:
+            coins.remove(positions_center)
+    return coins
+
+def create_special_coins(board):
+    special_coins = []
+
+    for position_corner in special_coins_pos:
+        special_coins.append(position_corner)
+
+    # TODO: Ajouter des coins aux positions spéciales, en utilisant la variable 'special_coins_pos'.
+    
+    return special_coins
